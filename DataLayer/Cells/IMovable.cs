38cdd134697a4baeeb6e45bbf4363cce35280236
Александр <aleksan-cygankov@yandex.ru>
2020-02-06 12:34:58 +0700

    Add  ICell IMovable, ISolid,

---
+++ DataLayer/Cells/IMovable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Cells
+{
+    public interface IMovable
+    {
+    }
+}
---
+++ DataLayer/Cells/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Cells
+{
+    public class Player : ICell, IMovable, ISolid
+    {
+    }
+}
